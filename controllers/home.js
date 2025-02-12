4eef3d631aa44ae5931376317b14b07fd0587bbf
Kabir Jaipal <73753258+kabirjaipal@users.noreply.github.com>
2025-02-12 23:49:25 +0530

    added workouts images

--- controllers/home.js
+++ controllers/home.js
@@ -8,7 +8,7 @@ import { prisma } from "../lib/prisma.js";
 export const getHome = async (req, res) => {
   // fetch 5 diets
   const diets = await prisma.m_diet.findMany({
-    take: 5,
+    take: 4,
     include: {
       benefits: true,
       meal_plans: true,
@@ -18,12 +18,12 @@ export const getHome = async (req, res) => {
 
   // fetch 5 gyms
   const gyms = await prisma.m_gym.findMany({
-    take: 5,
+    take: 4,
   });
 
   // fetch 5 workouts
   const workouts = await prisma.m_workout.findMany({
-    take: 5,
+    take: 4,
     include: {
       workout_plan: true,
       tips: true,
---
+++ prisma/migrations/20250212181018_added_image_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `m_workout` ADD COLUMN `image` VARCHAR(191) NOT NULL DEFAULT '';
